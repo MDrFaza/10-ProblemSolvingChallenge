57419c868bdbfda1255f365e3cb9429bce573715
MDrFaza <mdrfaza21@gmail.com>
2021-10-10 16:50:03 +0700

    make circle with constant velocity complete

---
+++ Problem Solving Challenge/Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+ 
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float initialForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+
+    void PushBall()
+    {
+        rigidBody2D.AddForce(new Vector2(initialForce, initialForce));
+    }
+}
