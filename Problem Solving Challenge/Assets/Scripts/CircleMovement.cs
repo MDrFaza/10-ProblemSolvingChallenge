12a3acfb20675bbd30838271f4b17d0d82879a48
MDrFaza <mdrfaza21@gmail.com>
2021-10-10 17:43:54 +0700

    circle movement control with arrow key

--- Problem Solving Challenge/Assets/Scripts/CircleMovement.cs
+++ Problem Solving Challenge/Assets/Scripts/CircleMovement.cs
@@ -8,18 +8,40 @@ public class CircleMovement : MonoBehaviour
     private Rigidbody2D rigidBody2D;
  
     // Besarnya gaya awal yang diberikan untuk mendorong bola
-    public float initialForceX;
-    public float initialForceY;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        PushBall();
     }
 
-    void PushBall()
+    void Update()
     {
-        rigidBody2D.AddForce(new Vector2(initialForceX, initialForceY));
+        Vector2 velocity = rigidBody2D.velocity;
+ 
+        if (Input.GetKey("up"))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            velocity.y = -speed;
+        }
+        else if(Input.GetKey("right"))
+        {
+            velocity.x = speed;
+        }
+        else if(Input.GetKey("left"))
+        {
+            velocity.x = -speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+            velocity.x = 0.0f;
+        }
+
+        rigidBody2D.velocity = velocity;
     }
 }
